b67cce720b1008a986429ea8719b3e0b5e4bb91b
EDXPO09 <105686779+EDXPO09@users.noreply.github.com>
2023-05-29 08:25:02 -0700

    Add files via upload

---
+++ Clase_2/cuadrado.py
@@ -0,0 +1,8 @@
+def cuadrado_entero(valor):
+    cuadrado = int(valor ** 2)
+    return cuadrado
+
+# Utilizando la funcion a partir de un valor predefinido
+valor_flotante = 3.5
+resultado = cuadrado_entero(valor_flotante)
+print("El Resultado es:", resultado)
